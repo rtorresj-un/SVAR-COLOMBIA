ae044051cb433c157810fc85077bdb48cd6b83a6
Raul Torres <rtorresj@unal.edu.co>
2020-11-30 01:51:48 -0500

    1:51

---
+++ Desagragación de series.R
@@ -0,0 +1,62 @@
+install.packages('tempdisagg')
+library(tseries)
+library(tempdisagg)
+IED<-data.frame(IED)
+
+m1 <- td(IED$Inversiones.de.cartera..entrada.neta.de.capital..balanza.de.pagos..US..a.precios.actuales. ~ 1, 
+         to = 12, method = "denton-cholette")
+plot(IED$Inversiones.de.cartera..entrada.neta.de.capital..balanza.de.pagos..US..a.precios.actuales.)
+plot(predict(m1))
+
+s1<-ts(predict(m1), start = c(1994,1), frequency = 12)
+ggplot2::autoplot(s1)
+
+m2 <- td(IED$Inversiones.de.cartera..bonos..PPG..PNG...NFL..US..a.precios.actuales. ~ 1, 
+         to = 12, method = "denton-cholette")
+plot(IED$Inversiones.de.cartera..bonos..PPG..PNG...NFL..US..a.precios.actuales.)
+plot(predict(m2))
+
+s2<-ts(predict(m2), start = c(1994,1), frequency = 12)
+ggplot2::autoplot(s2)
+
+m3 <- td(IED$Inversiones.de.cartera..neta..balanza.de.pagos..US..a.precios.actuales. ~ 1, 
+         to = 12, method = "denton-cholette")
+plot(IED$Inversiones.de.cartera..neta..balanza.de.pagos..US..a.precios.actuales.)
+plot(predict(m3))
+
+s3 <-ts(predict(m3), start = c(1994,1), frequency = 12)
+ggplot2::autoplot(s3)
+
+m4 <- td(IED$Inversión.extranjera.directa..entrada.neta.de.capital..balanza.de.pagos..US..a.precios.actuales. ~ 1, 
+         to = 12, method = "denton-cholette")
+plot(IED$Inversión.extranjera.directa..entrada.neta.de.capital..balanza.de.pagos..US..a.precios.actuales.)
+plot(predict(m4))
+
+s4<-ts(predict(m4), start = c(1994,1), frequency = 12)
+ggplot2::autoplot(s4)
+
+m5 <- td(IED$Inversión.extranjera.directa..neta..balanza.de.pagos..US..a.precios.actuales. ~ 1, 
+         to = 12, method = "denton-cholette")
+plot(IED$Inversión.extranjera.directa..neta..balanza.de.pagos..US..a.precios.actuales.)
+plot(predict(m5))
+
+s5<-ts(predict(m5), start = c(1994,1), frequency = 12)
+ggplot2::autoplot(s5)
+
+m6 <- td(IED$Inversión.extranjera.directa..salidas.netas..BDP..USD.a.precios.actuales. ~ 1, 
+         to = 12, method = "denton-cholette")
+plot(IED$Inversión.extranjera.directa..salidas.netas..BDP..USD.a.precios.actuales.)
+plot(predict(m6))
+
+s6<-ts(predict(m6), start = c(1994,1), frequency = 12)
+ggplot2::autoplot(s6)
+
+
+IED.da<-cbind(s1, s2, s3, s4, s5, s6)
+IED.da<-ts(IED.da, start = c(1994,1), frequency = 12)
+write.csv(IED.da, file = 'IED.da.csv')
+
+
+
+
+
